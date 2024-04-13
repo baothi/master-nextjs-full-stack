bd2e922f998bef342731102560c525fc54b2dc53
baothi <baothi246@gmail.com>
2024-04-13 15:50:12 +0700

    create components

--- app/page.js
+++ app/page.js
@@ -1,47 +1,32 @@
 'use client'
-function Card({ children }) {
-  return (
-    <div className="border rounded-md border-gray-600 p-4">
-      Card component: {children}
-    </div>
-  )
-}
+
 import React, { useState } from 'react';
+import Card from "@/components/card";
+
 export default function Home() {
-  // const name = 'Piotr'
+  
   // const [label, setLabel] = useState('Show');
   const [isVisible, setIsVisible] = useState(true);
   const [names, setNames] = useState(['Piotr', 'John', 'Terry'])
+  const name = 'Piotr'
   const handleClick = (e) =>{
     e.preventDefault();
     console.log(e);
     // setLabel(label == 'Show' ? 'Hide' : 'Show');
     setIsVisible(!isVisible)
   }
+
+  const handleAdd = (e) =>{
+    setNames([...names,'New element!'])
+  }
   
 
-  const cards = isVisible && names.map((name, index) => <Card key={name}> {name} </Card>)
-
-
-
+  const cards = isVisible && names.map((name, index) => <Card key={index}> {name} </Card>)
 
   return (
     <>
-      <div className="bg-gray-700">
-        Hello, {name}
-      </div>
-      <div className="flex justify-between mb-6">
-        <div className="p-2">Flex item</div>
-        <div className="p-2">Flex item</div>
-        <div className="p-2">Flex item</div>
-        <div className="p-2">Flex item</div>
-        <div className="p-2">Flex item</div>
-        <div className="p-2">Flex item</div>
-      </div>
-      <Card>This is being passed from the parent</Card>
-      <Card>
-        <div>This is JS!</div>
-        <Card>
+      <div className="p-20 space-y-4">
+          Hello, {names}
           <div className="flex justify-between mb-6">
             <div className="p-2">Flex item</div>
             <div className="p-2">Flex item</div>
@@ -50,12 +35,29 @@ export default function Home() {
             <div className="p-2">Flex item</div>
             <div className="p-2">Flex item</div>
           </div>
-        </Card>
-      </Card>
-      <Card />
-      <button onClick={handleClick}>
-        {isVisible ? 'Hide' : 'Show' }
-        </button>
+          <Card>This is being passed from the parent</Card>
+          <Card>
+            <div>This is JS!</div>
+            <Card>
+              <div className="flex justify-between mb-6">
+                <div className="p-2">Flex item</div>
+                <div className="p-2">Flex item</div>
+                <div className="p-2">Flex item</div>
+                <div className="p-2">Flex item</div>
+                <div className="p-2">Flex item</div>
+                <div className="p-2">Flex item</div>
+              </div>
+            </Card>
+          </Card>
+          <Card />
+          {cards}
+          <div className='flex space-x-4'>
+            <button onClick={handleClick}>
+              {isVisible ? 'Hide' : 'Show' }
+              </button>
+            <button onClick={handleAdd}>Add</button>
+          </div>
+      </div>
     </>
   );
 }
